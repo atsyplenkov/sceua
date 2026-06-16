bca156704edaa199830ad4619dee455f3075f9b1
Anatoly Tsyplenkov <s2@sent.com>
2026-06-16 11:36:27 +0300

    remove python

--- py/src/lib.rs
+++
@@ -1,9 +0,0 @@
-use pyo3::prelude::*;
-pub mod anime_compat;
-pub use anime_compat::*;
-
-#[pymodule]
-fn anime(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_class::<PyAnime>()?;
-    Ok(())
-}
